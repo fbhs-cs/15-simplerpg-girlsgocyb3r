7c0f2d90fd164754500c9d8b2570645f766668c5
Aaron <deadkilla67@gmail.com>
2020-01-30 13:13:20 -0600

    TestSpell

---
+++ TestSpell.java
@@ -0,0 +1,27 @@
+import jdk.javadoc.internal.tool.resources.javadoc;
+
+public class TestSpell{
+    public static void main(String[] args) {
+        Spell spell = new Spell(5, "Fire", 10);
+        Character devin = new Character("Devin", "hard plastic", 12);
+        
+
+
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
