0872537df855607a6ba3785a470b2d62a651e5fa
Aaron <deadkilla67@gmail.com>
2020-02-04 13:19:09 -0600

    Reaper is cool

--- Reaper.java
+++ Reaper.java
@@ -1,7 +1,34 @@
+import java.util.Scanner;
+
 public class Reaper extends Character{
+    Weapon weapon;
     public Reaper(String iname,int ixp){
-        super(iname, "Light",ixp);
+
+        super(iname, "Light" ,ixp,15);
+
+       
         
+    }
+    public void killSomeone(){
+        if(getHP() == 15);
+        else{
+            setHP(getHP() + 3);
+        }
+
+
+    }
+    
+
+    @Override
+    public void chooseAction(Character target){
+        
+
+
+
+        
+        
+
+
     }
 
 
