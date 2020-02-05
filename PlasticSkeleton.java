4d09e5b79afaffdcbef3bb6ccc1474147033053e
Devin Schwartz <devinschwartz9@gmail.com>
2020-02-05 13:00:48 -0600

    plastic skeleton chooseAction made

--- PlasticSkeleton.java
+++ PlasticSkeleton.java
@@ -1,5 +1,4 @@
 import java.util.Random;
-import java.util.Scanner;
 public  class PlasticSkeleton extends Character{
     public static String CLASS_NAME="Plastic skeleton";
     Weapon weapon;
@@ -11,7 +10,26 @@ public  class PlasticSkeleton extends Character{
     }
     public void chooseAction(Character target){
         Random rand=new Random();
-        int choice=rand.nextInt(3);
-        
+        int choice=rand.nextInt(2);
+        if (choice==0){
+            System.out.println("The plastic skeleton slices you with its scythe!");
+            weapon.attack(target);
+        }
+        if (choice==1){
+            System.out.println("The skeleton rattles its bones at you, reminding you that its plastic could take thousands of years to decompose.");
+            if (target.getSpeed()>2){
+                System.out.println("Demoralized, your speed falls.");
+                target.setSpeed(target.getSpeed()-2);
+            }
+            else{
+                System.out.println("Your speed can't go any lower, but you still take a moment to reflect on this.");
+            }
+            if(target.getMana()>1){
+                target.setMana(target.getMana()-2);
+                System.out.println("You lose two mana");
+            }
+
+            
+        }
     }
 }
