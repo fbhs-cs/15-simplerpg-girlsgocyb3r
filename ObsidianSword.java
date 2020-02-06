247c1f0c6cad31ccb0e2689f958fb2436dba04b7
Devin Schwartz <devinschwartz9@gmail.com>
2020-02-06 13:19:11 -0600

    Obsidian Sword started

---
+++ ObsidianSword.java
@@ -0,0 +1,33 @@
+public class ObsidianSword extends Weapon{
+    public ObsidianSword(int dmg,int crit){
+        super(dmg,"Dark", crit);
+        WEAPON_TYPE = "Sword";
+    }
+    @Override
+    public int attack(Character target){
+        double possible = Math.random();
+
+        if (target.isWeakTo(WEAPON_TYPE)){
+            if(possible <= this.getCrit() ){
+                target.takeDmg(this.getDmg() + 5 * 2);
+                System.out.println("The plastic does a lot of damage to you and the earth.");
+                return this.getDmg() + 5 * 2;
+            }
+            target.takeDmg(this.getDmg() + 5);
+            System.out.println("You are weak to plastic much like the earth.");
+            return this.getDmg() + 5;
+
+        }
+
+        else if (possible <= this.getCrit()){
+            target.takeDmg(this.getDmg() * 2);
+            System.out.println("Oh you didn't get lucky with this crit just like the earth not getting lucky with plastic");
+            return this.getDmg() * 2;
+        }
+        target.takeDmg(this.getDmg());
+        System.out.println("It only did base damage but it dealt massive damage to fish life.");
+        return this.getDmg();
+
+
+    }
+}
